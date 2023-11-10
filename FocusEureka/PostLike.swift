075a4c9a4adef3e5295b9d54ee521f8fbe408fbb
kaifeng yu <kaifeng.yu87@bcmail.cuny.edu>
2023-11-10 16:32:26 -0500

    add PostLike model

---
+++ FocusEureka/PostLike.swift
@@ -0,0 +1,15 @@
+//
+//  PostLike.swift
+//  FocusEureka
+//
+//  Created by kai on 11/2/23.
+//
+
+import Foundation
+
+struct PostLikes: Codable{
+    let user_id: Int
+    let post_id: Int
+    let user: Owner
+    
+}
