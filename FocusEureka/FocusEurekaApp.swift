56836fbf727e8c8a9c6bd7f51281377358e370f4
yuzchen7 <yuzchen7@gmail.com>
2023-11-11 17:28:54 -0500

    init basic view file

---
+++ FocusEureka/FocusEurekaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusEurekaApp.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusEurekaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
