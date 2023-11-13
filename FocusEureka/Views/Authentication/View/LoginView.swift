ee75107c3bf42fa914b16319177438da77abaf42
yuzchen7 <yuzchen7@gmail.com>
2023-11-12 20:46:10 -0500

    Migrating LoginView from testing project

---
+++ FocusEureka/Views/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 11/12/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
