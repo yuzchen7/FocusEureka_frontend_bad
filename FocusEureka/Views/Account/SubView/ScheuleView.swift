4b5f8380e06554e130449c978b158345dabc35b9
yuzchen7 <yuzchen7@gmail.com>
2023-11-12 21:05:03 -0500

    Migrating AccountView from testing project

---
+++ FocusEureka/Views/Account/SubView/ScheuleView.swift
@@ -0,0 +1,25 @@
+//
+//  ScheuleView.swift
+//  AuthTutorial
+//
+//  Created by yuz_chen on 11/7/23.
+//
+
+import SwiftUI
+
+struct ScheuleView: View {
+    var schedule: Schedule = Schedule()
+    
+    var body: some View {
+        VStack {
+            // single scheule view
+            ForEach(self.schedule.schedules) {element in
+                SingleScheuleView(day: element.day, isAvailable: element.isAvaliable)
+            }
+        }
+    }
+}
+
+#Preview {
+    ScheuleView()
+}
