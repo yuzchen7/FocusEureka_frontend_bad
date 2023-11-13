1a29382b789133f24eafbd7526295fff59cf9e42
yuzchen7 <yuzchen7@gmail.com>
2023-11-12 20:56:53 -0500

    Migrating SettingRowView from testing project

--- FocusEureka/Components/SettingRowView.swift
+++ FocusEureka/Components/SettingRowView.swift
@@ -1,18 +1,32 @@
 //
 //  SettingRowView.swift
-//  FocusEureka
+//  AuthTutorial
 //
-//  Created by yuz_chen on 11/12/23.
+//  Created by yuz_chen on 10/26/23.
 //
 
 import SwiftUI
 
 struct SettingRowView: View {
+    var image: String;
+    var title: String;
+    var tinColor: Color;
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 12) {
+            Image(systemName: self.image)
+                .imageScale(.small)
+                .font(.title)
+                .foregroundColor(self.tinColor)
+            
+            Text(self.title)
+                .font(.subheadline)
+                .foregroundStyle(.black)
+        }
     }
 }
 
 #Preview {
-    SettingRowView()
+    SettingRowView(image: "gear", title: "Version", tinColor: Color(.systemGray))
 }
+
