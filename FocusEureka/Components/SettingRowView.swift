ade4ae6816f4776ad821ccc042674727be6e96ab
yuzchen7 <yuzchen7@gmail.com>
2023-11-12 20:56:22 -0500

    Migrating SettingRowView from testing project

---
+++ FocusEureka/Components/SettingRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingRowView.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 11/12/23.
+//
+
+import SwiftUI
+
+struct SettingRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingRowView()
+}
